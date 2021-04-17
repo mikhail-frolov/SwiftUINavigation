d952462572606b85bb487a11c50439cf43b1fd42
Mikhail <mishafrolov882000@gmail.com>
2021-04-17 18:18:02 +0600

    Initial Commit

---
+++ SwiftUINav/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINav
+//
+//  Created by Mikhail Frolov on 2021-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUINav/SwiftUINavApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavApp.swift
+//  SwiftUINav
+//
+//  Created by Mikhail Frolov on 2021-04-17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
